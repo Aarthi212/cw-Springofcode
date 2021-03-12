3f605ab8f878b9c6814fede4197680e659c32086
Aarthi212 <70945036+Aarthi212@users.noreply.github.com>
2021-03-12 10:46:51 +0530

    Add files via upload

---
+++ day6.c
@@ -0,0 +1,54 @@
+#include <stdio.h> 
+
+  
+
+void rotate(int arr[], int n) 
+
+{ 
+
+   int x = arr[n-1], i; 
+
+   for (i = n-1; i > 0; i--) 
+
+      arr[i] = arr[i-1]; 
+
+   arr[0] = x; 
+
+} 
+
+  
+
+int main() 
+
+{ 
+
+    int arr[] = {1, 2, 3, 4, 5,6,7,8}, i; 
+
+    int n = sizeof(arr)/sizeof(arr[0]); 
+
+  
+
+    printf("Given array is   "); 
+
+    for (i = 0; i < n; i++) 
+
+        printf("%d ", arr[i]); 
+
+  
+
+    rotate(arr, n); 
+
+  
+
+    printf("\nRotated array is "); 
+
+    for (i = 0; i < n; i++) 
+
+        printf("%d ", arr[i]); 
+
+  
+
+    return 0; 
+
+}
+
